d76480b348e6aae932bd0040dde9d909d758c422
Shreya Maheshwari <shreya.maheshwari.2000@gmail.com>
2024-01-21 20:43:55 +0530

    Save tab

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
 let myLeads = [];
+let tabs = [{ url: "https://leetcode.com/problemset/" }];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const saveBtn = document.getElementById("save-btn");
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-inputBtn.addEventListener("click", function () {
-  //Read input value
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
 
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     //Way 1
     // const li = document.createElement("li");
-    // li.textContent = myLeads[i];
+    // li.textContent = leads[i];
     // ulEl.append(li);
 
     //Way 2
     listItems += `
       <li>
-        <a target='_blank' rel='noopener noreferrer' href='${myLeads[i]}'>
-          ${myLeads[i]}
+        <a target='_blank' rel='noopener noreferrer' href='${leads[i]}'>
+          ${leads[i]}
         </a>
       </li>`;
   }
 
   ulEl.innerHTML = listItems;
 }
+
+function saveLead(leadValue) {
+  myLeads.push(leadValue);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+}
+
+inputBtn.addEventListener("click", () => saveLead(inputEl.value));
+
+saveBtn.addEventListener("dblclick", function () {
+  //Get current tab
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    let url = tabs[0].url;
+    saveLead(url);
+  });
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
